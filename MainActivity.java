5dfd7a58bed9b242c842d3a5262e18c713bd623a
Nihal <30835109+TheNihal@users.noreply.github.com>
2023-05-23 02:22:45 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public EditText input;
+    public Button convert;
+    public Button button1;
+    public TextView output;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+
+        input=findViewById(R.id.inputid);
+        convert=findViewById(R.id.buttonid);
+        output=findViewById(R.id.outputid);
+
+        button1=findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replace(new bmi());
+            }
+        });
+
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num= Integer.parseInt(input.getText().toString());
+                int usd= num*107;
+                output.setText(num+" BDT= "+usd+"$");
+            }
+        });
+
+    }
+
+    private void replace(Fragment bmi) {
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutID,bmi);
+        fragmentTransaction.commit();
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fm=getSupportFragmentManager();
+        FragmentTransaction ft= fm.beginTransaction();
+        ft.replace(R.id.frameLayoutID,fragment);
+        ft.commit();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
